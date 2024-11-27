68dd4ae4c364ea0925d37fb59264a7c1eab4caa5
Ilija Ivic <ilijaivic17@gmail.com>
2024-11-27 12:37:36 +0100

    Time: 0 ms (100.00%) | Memory: 53.1 MB (5.58%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -0,0 +1,46 @@
+function strStr(haystack: string, needle: string): number {
+    if (needle.length === 0) return 0
+  
+    const buildLPS = (pattern: string) => {
+      const lps: number[] = Array(pattern.length).fill(0)
+      let length = 0;
+      let i = 1
+  
+      while (i < pattern.length) {
+        if (pattern[i] === pattern[length]) {
+          length++
+          lps[i] = length
+          i++
+        } else if (length !== 0) {
+            length = lps[length - 1]
+          } else {
+            lps[i] = 0
+            i++
+          }
+      }
+      return lps
+    };
+  
+    const lps = buildLPS(needle);
+    let i = 0
+    let j = 0
+  
+    while (i < haystack.length) {
+      if (haystack[i] === needle[j]) {
+        i++
+        j++
+      }
+  
+      if (j === needle.length) {
+        return i - j
+      } else if (i < haystack.length && haystack[i] !== needle[j]) {
+        if (j !== 0) {
+          j = lps[j - 1]
+        } else {
+          i++
+        }
+      }
+    }
+  
+    return -1
+  }
