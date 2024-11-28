c85bcbedc01e24f1748b280f21df9e0c83bddff1
Ilija Ivic <ilijaivic17@gmail.com>
2024-11-28 13:12:48 +0100

    Time: 0 ms (100.00%) | Memory: 53.3 MB (80.31%) - LeetSync

---
+++ 342-power-of-four/power-of-four.ts
@@ -0,0 +1,12 @@
+  function isPowerOfFour(n: number) {
+    if (n === 1 || n === 4) return true
+    if (n % 4 !== 0) return false
+    
+    let sum = 4
+    const limit = n / 4
+    while(sum <= limit) {
+      if (sum === limit) return true
+      sum *= 4
+    }
+    return false;
+  }
