0ebcab38db69c8a2f1a3bd8e4a1b8cea06e2a401
Ilija Ivic <ilijaivic17@gmail.com>
2024-12-07 13:30:37 +0100

    Time: 9 ms (99.46%) | Memory: 71.1 MB (40.59%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.ts
+++ 219-contains-duplicate-ii/contains-duplicate-ii.ts
@@ -1,15 +1,15 @@
   function containsNearbyDuplicate(nums: number[], k: number) {
-    const map = new Map();
+    const map = new Map()
 
-    for (let i = 0; i < nums.length; i++) {
-      const num = nums[i];
+    for (let index = 0; index < nums.length; index++) {
+      const num = nums[index]
       
       if (map.has(num)) {
         const prevIndex = map.get(num)
-        if (Math.abs(i - prevIndex) <= k) return true
+        if (Math.abs(index - prevIndex) <= k) return true
       }
-      
-      map.set(num, i)
+
+      map.set(num, index)
     }
 
     return false
