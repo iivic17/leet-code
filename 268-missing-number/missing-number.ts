e6e08d7498176aebc6248b0ea21329409dcc1f41
Ilija Ivic <ilijaivic17@gmail.com>
2024-12-14 20:42:49 +0100

    Time: 2 ms (59.59%) | Memory: 53 MB (27.80%) - LeetSync

--- 268-missing-number/missing-number.ts
+++ 268-missing-number/missing-number.ts
@@ -1,21 +1,21 @@
 function missingNumber(nums: number[]) {
-    let lengthFactorial = 1n
+    let lengthFactorial = 1
 
     for (let i = 2; i <= nums.length; i++) {
-        lengthFactorial *= BigInt(i)
+        lengthFactorial += i
     }
 
-    let perpetrator = 1n
+    let perpetrator = 0
     let hasZero = false
 
     for (const num of nums) {
         if (num !== 0) {
-            perpetrator *= BigInt(num)
+            perpetrator += num
         } else {
             hasZero = true
         }
     }
 
     if (!hasZero) return 0
-    return Number(lengthFactorial / perpetrator)
+    return lengthFactorial - perpetrator
 }
