40c424ca5ab71237e4e5754fa5f65edeb249ff47
Ilija Ivic <ilijaivic17@gmail.com>
2024-12-19 15:32:17 +0100

    Time: 7 ms (20.25%) | Memory: 55.3 MB (8.86%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.ts
+++ 414-third-maximum-number/third-maximum-number.ts
@@ -1,19 +1,18 @@
 function thirdMax(nums: number[]) {
-    if (nums.length === 1) return nums[0]
-    if (nums.length === 2) return nums[0] > nums[1] ? nums[0] : nums[1]
+    const uniqueNums = Array.from(new Set(nums))
+    if (uniqueNums.length < 3) return Math.max(...uniqueNums)
 
-    let first = -Infinity, second = -Infinity, third = -Infinity
+    let [first, second, third] = [-Infinity, -Infinity, -Infinity]
 
-    for (let i = 0; i < nums.length; i++) {
-        const curr = nums[i]
-        const min = Math.min(first, second, third)
-
-        if (curr > first && first === min && second !== curr && third !== curr) first = curr
-        else if (curr > second && second === min  && first !== curr && third !== curr) second = curr
-        else if (curr > third && third === min  && second !== curr && first !== curr) third = curr
+    for (const num of uniqueNums) {
+        if (num > first) {
+            [first, second, third] = [num, first, second]
+        } else if (num > second) {
+            [second, third] = [num, second]
+        } else if (num > third) {
+            third = num
+        }
     }
 
-    if (first === -Infinity || second === -Infinity || third === -Infinity) return Math.max(first, second, third)
-    if (first !== second && first !== third && second !== third) return Math.min(first, second, third)
-    return Math.max(first, second, third)
+    return third
 }
